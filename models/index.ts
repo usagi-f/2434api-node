0acccbf7abe4e6cc4667a35d3d15835578cfadda
usagi.f@gmail.com <usagi.f@gmail.com>
2018-07-30 17:40:35 +0900

    fix return obj

--- models/index.ts
+++ models/index.ts
@@ -1,7 +1,7 @@
 import UserModel, { UserData } from './UserModel'
 import data from '../data/user'
 
-const records = data.map((record: UserData) => new UserModel(record))
+const records = data.map((record: UserData) => new UserModel(record).toObject())
 
 const findAll = (cb: Function) => cb(null, records)
 
